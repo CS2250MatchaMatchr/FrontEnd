6f2fd3ab9c4a2220691be99bc61ffceac10df77c
Daniel Pasion <dnpaxion@gmail.com>
2023-11-19 11:50:32 -0800

    Setup middleware connection allowing user input from email & password to be stored into database

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
+//Express
 const express = require('express')
 const app = express()
+
+//Json format responses
 app.use(express.json());
+
+//Database
 const db = require('./models')
+
+//Middleware
+var cors = require('cors')
+app.use(cors())
+
 
 
 //Routers 
 const hackerRouter = require('./routes/Hackers')
 app.use("/hackers",hackerRouter);
 
+
+//Running the app + db
 db.sequelize.sync().then(() => {
     app.listen(5000, () => {console.log("Server started on port 5000")});
 });
