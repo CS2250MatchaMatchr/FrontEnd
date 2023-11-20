09433f8a7dccab7d01eae8fac594ef2cdb56c097
Daniel Pasion <dnpaxion@gmail.com>
2023-11-20 09:17:01 -0800

    Created all three databases, fixed route to store who the user is once logged in

--- server/routes/Hackers.js
+++ server/routes/Hackers.js
@@ -10,7 +10,7 @@ router.get("/", async (req,res) =>{
     res.json(listOfPosts)
 });
 
-//Gets hacker's password given email for login authentication
+//Gets hacker's password for login authentication (email needed)
 router.get("/getPassword", async (req,res) => {
     const inputEmail = req.query.email;
     const inputPassword = req.query.hackerPassword;
@@ -26,17 +26,34 @@ router.get("/getPassword", async (req,res) => {
             res.send("Incorrect Password");
         }
         else{
-            res.send(true);
+            const hackerID = await sequelize.query("SELECT id FROM `Hackers` WHERE email = :email", 
+                { 
+                    replacements: { email: inputEmail}, 
+                    type: QueryTypes.SELECT
+                });
+            res.send(String(hackerID[0].id));
         } 
     } catch (error){
         res.send("Email does not exist");
     }
 });
 
-// //Creates a Hacker Object if not in table and makes it so that if the email already exists in the database, then it sends an error message
+// //Creates the hacker object, email and password only
 router.post("/", async (req, res) => {
     const inputEmail = req.body.email;
-    const hacker = req.body;
+    const hacker = {
+        email: inputEmail,
+        hackerPassword: req.body.hackerPassword,
+        fullName: "",
+        classStanding: "",
+        gender: "",
+        school: "",
+        frontOrBackEnd: "",
+        github: "",
+        linkedIn: "",
+        biography: "",
+        lookingForTeam: true
+    };
     const sqlStatement = await sequelize.query("SELECT DISTINCT email FROM `Hackers` WHERE email = :email", 
     { 
         replacements: { email: inputEmail}, 
@@ -45,16 +62,43 @@ router.post("/", async (req, res) => {
     console.log(sqlStatement);
     try{
         const dbEmail = sqlStatement[0].email;
-        // if (inputEmail === dbEmail) {
         res.send("Fail");
         
     } 
     catch (error) {
         await Hackers.create(hacker);
-        res.send("Success");
+        const hackerID = await sequelize.query("SELECT id FROM `Hackers` WHERE email = :email", 
+        { 
+            replacements: { email: inputEmail}, 
+            type: QueryTypes.SELECT
+        });
+        res.send(String(hackerID[0].id));
     }
     
     
+    
+})
+
+router.put("/", async (req, res) => {
+
+    const sqlStatement = await sequelize.query("UPDATE HACKER SET fullName = :fullName, classStanding = :classStanding, gender = :gender, school = :school," + 
+                                               "frontOrBackEnd = :frontOrBackEnd, github = :github, linkedIn = :linkedIn, biography = :biography, lookingForTeam = :lookingForTeam" +
+                                               "WHERE id = ",
+
+        {
+            replacements: { fullName: req.body.fullName,
+                            classStanding: req.body.classStanding,
+                            gender: req.body.gender,
+                            school: req.body.school,
+                            frontOrBackEnd: req.body.frontOrBackEnd,
+                            github: req.body.github,
+                            linkedIn: req.body.linkedIn,
+                            biography: req.body.biography,
+                            lookingForTeam: Boolean(req.body.lookingForTeam)},
+            type: QueryTypes.SELECT 
+        });
+
+
     
 })
 
