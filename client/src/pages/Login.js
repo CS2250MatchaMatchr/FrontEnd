7750d6cd7010475d2c69ace6f8674a3d6d211abe
Jade Nguyen <jade@Jades-MacBook-Pro-2.local>
2023-11-19 23:59:50 -0800

    Changed Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,52 @@
 import { Link } from 'react-router-dom'
+import { Container , } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import React, {useEffect,useState} from 'react'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+import axios from "axios";
 
 
+export default function Login() {
 
-export default function Login () {
+    const initialValues = {
+        email: "",
+        hackerPassword: ""
+    };
+
+    const onSubmit = (data => {
+        /*axios.post("http://localhost:5000/hackers", data).then((response) => {
+            console.log(data)
+        });*/
+        console.log(data)
+    });
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().required("You must enter an email!"),
+        hackerPassword: Yup.string().required().min(5)
+    })
+
+
     return(
         <>
-            <h2>Login Page</h2>
-            <Link to="/CreateAccount">Don't have an account? Create one here!</Link>
+            <h1>Matchr</h1>
+            <Container>
+            <h2>Sign In</h2>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                <Form>
+                    <ErrorMessage name="email" component="span"/>
+                    <Field name="email" placeholder="Email"/>
+                    <br></br>
+                    <ErrorMessage name="hackerPassword" component="span"/>
+                    <Field name="hackerPassword" placeholder="Password"/>
+                    <br></br>
+                    <button type="submit" component="span">Sign in!</button>
+                </Form>
+            </Formik>
+            Don't have an account?
+            <br></br>
+            <Link to="/CreateAccount">Create one here!</Link>
+            </Container>
         </>
     )
 }
-
