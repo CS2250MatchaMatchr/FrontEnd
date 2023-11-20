f8578c447d7d97f02ad761398a2781b564daf046
Daniel Pasion <dnpaxion@gmail.com>
2023-11-20 01:43:00 -0800

    Setup password validation routes

--- server/routes/Hackers.js
+++ server/routes/Hackers.js
@@ -1,18 +1,39 @@
 const express = require('express')
 const router = express.Router()
 const { Hackers, sequelize } = require("../models")
+const { QueryTypes } = require('sequelize');
 
+
+//Returns a JSON of all Hackers
 router.get("/", async (req,res) =>{
     const listOfPosts = await Hackers.findAll()
     res.json(listOfPosts)
 });
 
-router.post("/", async (req,res) => {
-    await Hackers.create(hacker)
-    res.json(hacker)
+//Gets hacker's password given email for login authentication
+router.get("/getPassword", async (req,res) => {
+    const inputEmail = req.body.email;
+    const inputPassword = req.body.hackerPassword;
+    const sqlStatement = await sequelize.query("SELECT hackerPassword FROM `Hackers` WHERE email = :email" , 
+        { 
+            replacements: {email: inputEmail},
+            type: QueryTypes.SELECT 
+        });
+
+    try {
+        dbPassword = sqlStatement[0].hackerPassword;
+        if (dbPassword != inputPassword){
+            res.send("Incorrect Password");
+        }
+        else{
+            res.send(true);
+        } 
+    } catch (error){
+        res.send("Email does not exist");
+    }
 });
 
-// make it so that if the email already exists in the database, then it sends an error message
+// //Creates a Hacker Object if not in table and makes it so that if the email already exists in the database, then it sends an error message
 router.post("/", async (req, res) => {
     const inputEmail = req.body.email;
     const dbEmail = await sequelize.query("SELECT email FROM MatchaMatchr.Hackers WHERE email = :email", { replacements: { email: inputEmail}});
