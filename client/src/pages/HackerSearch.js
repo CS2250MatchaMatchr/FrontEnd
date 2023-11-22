9324712866825ff836fee0ad0456963947df7010
Daniel Pasion <dnpaxion@gmail.com>
2023-11-22 13:24:31 -0800

    Set up file for hacker and team search

---
+++ client/src/pages/HackerSearch.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+export default function NoPage() {
+    return(
+        <>
+        <Header></Header>
+            <h1>Error 404: Not found</h1>
+        </>
+    )
+}
