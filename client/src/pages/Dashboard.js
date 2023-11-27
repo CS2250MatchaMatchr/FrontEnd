4c9699879f14a77341ca79c496b2ef3ca2430010
nochyq <cynthian2@cpp.edu>
2023-11-26 19:44:05 -0800

    dashboard css

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,24 @@
 import Header from "../components/Header";
+import "../styles/dashboard.css";
 
 export default function Dashboard() {
-    return(
+    return (
         <>
             <Header />
-            <div>
-                
-            </div>
-            <div>
-
-            </div>
-            <div>
-
+            <br />
+            <div class="container">
+                <div class="column">
+                    <h2>Profile</h2>
+                    <p> This is first column of our grid system</p>
+                </div>
+                <div class="column">
+                    <h2>Team</h2>
+                    <p> This is second column of our grid system</p>
+                </div>
+                <div class="column">
+                    <h2>Countdown</h2>
+                    <p> This is third column of our grid system</p>
+                </div>
             </div>
         </>
     )
