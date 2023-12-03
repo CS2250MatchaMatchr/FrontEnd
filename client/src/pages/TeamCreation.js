61a4a2cc585e926714ee3c35a05cdc7e50fade8d
Jade Nguyen <jade@b98-aruba-authenticated-10-110-156-219.wlan.cpp.edu>
2023-12-03 15:02:28 -0800

    Added sponsors to dashboard, made css for create team, changed profile

--- client/src/pages/TeamCreation.js
+++ client/src/pages/TeamCreation.js
@@ -67,17 +67,20 @@ export default function TeamCreation() {
     return (
         <>
             <Header></Header>
+            <div className='urmom'>
+                <h2 className='createTeam'>Create Team!</h2>
             <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                <Form>
-                    <label>Enter desired team name: </label>
-                    <Field name="teamName"/>
+                <Form className='teamForm'>
+                    <label className='teamLabel'>Enter desired team name: </label>
+                    <Field className="teamName" name="teamName"/>
                     <br></br>
-                    <button>Create new team!</button>
+                    <button className='teamButton'>Create new team!</button>
                     <br></br>
                 </Form>
             </Formik>
-            <div>Passcode used to invite hackers to your team: <b>{passcodeTeam}</b></div>
-            <Link to="/Dashboard">Back</Link>
+            <div className='password'>Passcode used to invite hackers to your team: <b>{passcodeTeam}</b></div>
+            <div className='link'><Link to="/Dashboard">Back</Link></div>
+            </div>
         </>
     )
 }
