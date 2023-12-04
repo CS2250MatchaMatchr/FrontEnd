2eee609d9e113138e08252deedbbca25893bd8f8
jadethegemstone <jnn0604@gmail.com>
2023-12-03 21:06:35 -0800

    changed team management

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,25 +1,7 @@
 import Header from "../components/Header";
 import "../styles/profile.css";
-import axios from "axios";
-import React, {useEffect,useState} from 'react'
 
 export default function Profile() {
-
-    const [userJson,setUserJson] = useState([]);
-
-    useEffect(() => {
-        const hackerID = localStorage.getItem(localStorage.key("hackerID"));
-        console.log(hackerID)
-        let url = "http://localhost:5001/hackers?id=" + hackerID
-        axios.get(url).then(async (response) => {
-            console.log(response);
-            setUserJson(response.data[0][0])
-            console.log(userJson)
-        });
-    },[]);
-
-    
-
     return (
         <>
             <Header />
@@ -28,14 +10,15 @@ export default function Profile() {
                 <h2>Profile</h2>
                 
                 <div className="info">
-                    <p>email: {userJson.email}</p>
-                    <p>fullName: {userJson.fullName}</p>
-                    <p>classStanding: {userJson.classStanding}</p>
-                    <p>gender: {userJson.gender}</p>
-                    <p>frontOrBackEnd: {userJson.frontOrBackEnd}</p>
-                    <p>github: {userJson.github}</p>
-                    <p>linkedin: {userJson.linkedin}</p>
-                    <p>biography: {userJson.biography}</p>
+                    <p>email</p>
+                    <p>fullName</p>
+                    <p>classStanding</p>
+                    <p>gender</p>
+                    <p>gender</p>
+                    <p>frontOrBackEnd</p>
+                    <p>github</p>
+                    <p>linkedin</p>
+                    <p>biography</p>
                 </div>
                 <br />
                 <button className="editButton">Edit</button>
