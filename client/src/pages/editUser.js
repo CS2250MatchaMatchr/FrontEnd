970c08878ebeae43d1e6a1273f5eba74d938c342
Daniel Pasion <dnpaxion@gmail.com>
2023-12-05 03:42:50 -0800

    Bruh

--- client/src/pages/editUser.js
+++ client/src/pages/editUser.js
@@ -33,7 +33,7 @@ export function User() {
     data["lookingForTeam"] = true
     data["id"] = hackerID
     console.log(data);
-    axios.put("http://localhost:5001/hackers", data).then((response) => {
+    axios.put("http://localhost:5001/hackers/edit", data).then((response) => {
       if (response.data == "Update Succesful") {
         navigate("/EditTechnologies")
       }
