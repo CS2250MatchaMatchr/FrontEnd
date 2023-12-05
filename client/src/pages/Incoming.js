0e28cbf4223f9ce7efd7c9e9bff9595ed7084b15
trucnnguyen <nnttruc2609@gmail.com>
2023-12-05 02:12:09 -0800

    Final One OMG, incoming is doneeeee

--- client/src/pages/Incoming.js
+++ client/src/pages/Incoming.js
@@ -6,6 +6,7 @@ import { Button } from 'react-bootstrap'
 import { Dropdown } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import { useNavigate, Link } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 import "../styles/HackerSearch.css";
 import "../styles/incomingMessage.css";
 
@@ -13,6 +14,7 @@ export default function Incoming() {
 
     const [listOfMessages,getListOfMessages] = useState()
     const [prepareHTML,setprepareHTML] = useState([<></>])
+    const [goback, setGoback] = React.useState(false);
 
     useEffect(() => {
         const hackerID = localStorage.getItem(localStorage.key("hackerID"));
@@ -34,31 +36,39 @@ export default function Incoming() {
                     axios.get(url2).then((response) => {
                         console.log(response)
                         htmlResponse.push(<div>
-                            <div>Time Sent: {listOfMessages[i].createdAt}</div>
-                                <div><b>{sender.fullName}Sent You A Message</b>: {listOfMessages[i].message}</div>
+                            <div style={{ border: '1px solid #ccc', padding: '5px', marginBottom: '15px', marginRight: '15px', borderRadius:'20px' }}>Time Sent: {listOfMessages[i].createdAt}
+                                <div><b>{sender.fullName}</b> sent You A Message: <br/><div className="message">{listOfMessages[i].message}</div></div>
+                                <br></br>
+                                <img src={response.data} style={{ maxWidth: '70px', height: 'auto'}}></img>
                                 <br></br>
                                 <br></br>
-                                <img src={response.data}></img>
-                            </div>)
+                            </div>
+                        </div>)
                     });
             });
         }
         setprepareHTML(htmlResponse);
     }
+
+    if (goback === true) {
+        return <Navigate to="/Inbox" />;
+    }
     return(
         <>
-            
             <Header />
             <br />
-            <div classname="profile">
+            <div className="profile">
                 <h1>Incoming Messages</h1>
-                <div classname="editButton">
+                <div className="editButton">
                     <button onClick={onSubmit}>Load Data</button>
                 </div>
-                <Link to="/Inbox" >Back</Link>
+                <br />
+                <div classname="backButton">
+                    <button onClick={() => { setGoback(true); }}>Go Back</button>
+                </div>
                 <div className="results">
                     {prepareHTML.map((value, key) => {
-                        return (<>{value}</>)
+                        return <React.Fragment key={key}>{value}</React.Fragment>;
                     })}
                 </div>
             </div>
