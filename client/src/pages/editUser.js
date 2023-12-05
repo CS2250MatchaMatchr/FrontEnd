1f423d5172c16f50a138d633d3f8258a01d1c619
Daniel Pasion <dnpaxion@gmail.com>
2023-12-05 03:37:36 -0800

    FUCK THIS PROJECT

--- client/src/pages/editUser.js
+++ client/src/pages/editUser.js
@@ -8,41 +8,41 @@ import { Button } from 'react-bootstrap';
 import { useNavigate, Link } from 'react-router-dom'
 import "../styles/user.css";
 
-  export function EditUser() {
-     //State to allow the localStorage of DB ID
-    const [hackerID, setID] = useState([-1]);
-    let navigate = useNavigate()
+export function User() {
+  //State to allow the localStorage of DB ID
+  const [hackerID, setID] = useState([-1]);
+  let navigate = useNavigate()
 
-    //useEffect Used to store data into localStorage
-    useEffect(() => {
-        setID(localStorage.getItem('hackerID'));
-    });
+  //useEffect Used to store data into localStorage
+  useEffect(() => {
+    setID(localStorage.getItem('hackerID'));
+  });
 
-      const initialValues = {
-      fullName: '',
-      classStanding: '',
-      gender: '',
-      school: '',
-      lookingForTeam: true,
-      frontOrBackEnd: "",
-      github: '',
-      linkedIn: '',
-    };
+  const initialValues = {
+    fullName: '',
+    classStanding: '',
+    gender: '',
+    school: '',
+    lookingForTeam: true,
+    frontOrBackEnd: "",
+    github: '',
+    linkedIn: '',
+  };
 
-    const onSubmit = (data => {
-      data["lookingForTeam"] = true
-      data["id"] = hackerID
-      console.log(data);
-      axios.put("http://localhost:5001/hackers",data).then((response) => {
-        if (response.data == "Update Succesful"){
-          navigate("/EditTechnologies")
-        }
-        else{
-          alert("An error has occured, please try again with a different email")
-          navigate("/")
-        }
-      })
-    });
+  const onSubmit = (data => {
+    data["lookingForTeam"] = true
+    data["id"] = hackerID
+    console.log(data);
+    axios.put("http://localhost:5001/hackers", data).then((response) => {
+      if (response.data == "Update Succesful") {
+        navigate("/EditTechnologies")
+      }
+      else {
+        alert("An error has occured, please try again with a different email")
+        navigate("/")
+      }
+    })
+  });
 
   const validationSchema = Yup.object().shape({
     fullName: Yup.string().required('Full Name is required'),
@@ -53,12 +53,7 @@ import "../styles/user.css";
     frontOrBackEnd: Yup.string().required('Frontend/Backend selection is required'),
     github: Yup.string().required('Github link is required'),
     linkedIn: Yup.string()
-
-});
-
-const errortext = {
-  color: 'red'
-}
+  });
   return (
     <div className="container mt-5">
       <div className="matchaHeader">
@@ -71,12 +66,12 @@ const errortext = {
           <Form>
             <div className="mb-3">
               <label className="form-label">Enter Full Name:</label>
-              <ErrorMessage name="fullName" component="span" style={errortext}/>
+              <ErrorMessage name="fullName" component="span" />
               <Field className="form-control" name="fullName" placeholder="ex: John Smith" />
             </div>
             <div className="mb-3">
               <label>What Is Your Class Standing?</label>
-              <ErrorMessage name="classStanding" component="span" style={errortext}/>
+              <ErrorMessage name="classStanding" component="span" />
               <div role="group" aria-labelledby="class-standing-label">
                 <label id="class-standing-label">
                   <Field type="radio" name="classStanding" value="freshman" />
@@ -98,7 +93,7 @@ const errortext = {
             </div>
             <div className="mb-3">
               <label>What Is Your Gender?</label>
-              <ErrorMessage name="gender" component="span" style={errortext} />
+              <ErrorMessage name="gender" component="span" />
               <div role="group" aria-labelledby="gender-label">
                 <label id="gender-label">
                   <Field type="radio" name="gender" value="male" />
@@ -116,42 +111,42 @@ const errortext = {
             </div>
             <div className="mb-3">
               <label className="form-label">What Is Your School?</label>
-              <ErrorMessage name="school" component="span" style={errortext}/>
+              <ErrorMessage name="school" component="span" />
               <Field className="form-control" name="school" placeholder="ex: Cal Poly Pomona" />
 
-                <label className="form-label">Frontend or Backend?</label>
-                <ErrorMessage name="frontOrBackEnd" component="span" style={errortext}/>
-                <div role="group" aria-labelledby="front-back-label">
-                  <label id="front-back-label">
-                    <Field type="radio" name="frontOrBackEnd" value="frontend" />
-                    Frontend
-                  </label>
-                  <label>
-                    <Field type="radio" name="frontOrBackEnd" value="backend" />
-                    Backend
-                  </label>
-                  <label>
-                    <Field type="radio" name="frontOrBackEnd" value="fullstack" />
-                    Fullstack
-                  </label>
-                </div>
-            </div> 
-            <label className="form-label">Github Link:</label> 
-            <ErrorMessage name="Github" component="span" style={errortext}/>
+              <label className="form-label">Frontend or Backend?</label>
+              <ErrorMessage name="frontOrBackEnd" component="span" />
+              <div role="group" aria-labelledby="front-back-label">
+                <label id="front-back-label">
+                  <Field type="radio" name="frontOrBackEnd" value="frontend" />
+                  Frontend
+                </label>
+                <label>
+                  <Field type="radio" name="frontOrBackEnd" value="backend" />
+                  Backend
+                </label>
+                <label>
+                  <Field type="radio" name="frontOrBackEnd" value="fullstack" />
+                  Fullstack
+                </label>
+              </div>
+            </div>
+            <label className="form-label">Github Link:</label>
+            <ErrorMessage name="Github" component="span" />
             <Field className="form-control" name="github" placeholder="ex: https://github.com/username" />
 
             <label className="form-label">LinkedIn Profile:</label>
-            <ErrorMessage name="linkedIn" component="span" style={errortext}/>
+            <ErrorMessage name="linkedIn" component="span" />
             <Field className="form-control" name="linkedIn" placeholder="ex: https://www.linkedin.com/in/yourprofile" />
             <label className="form-label">Tell us about yourself:</label>
-            <ErrorMessage name="linkedIn" component="span" style={errortext}/>
+            <ErrorMessage name="linkedIn" component="span" />
             <Field className="form-control" name="biography" placeholder="ex: https://www.linkedin.com/in/yourprofile" />
-    
-            <button type="submit" className="updateButton">Update User Profile!</button>
+
+            <Button type="submit" className="button-success">Publish Changes</Button>
           </Form>
         </Formik>
       </div>
     </div>
   );
 };
-export default EditUser;
+export default User;
