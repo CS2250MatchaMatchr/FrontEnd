1faa6ddcb9bcfc31f65aad25430a8c9bcee2fef9
Caleb C <kaichun24@gmail.com>
2023-12-06 13:35:57 -0800

    stuff

--- client/src/pages/HackerSearch.js
+++ client/src/pages/HackerSearch.js
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import { Formik, Form, Field, FieldProps } from 'formik'
 import axios from "axios";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { Dropdown } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
